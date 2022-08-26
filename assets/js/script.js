4d9376866456d4f6ac658a601fcd53709a1d25c4
Thomas Davidson <tomd1969@gmail.com>
2022-08-26 04:33:48 -0400

    Final assignment for introductory course. Hooray.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,41 @@
 // TODO: Declare any global variables we need
 
-
+let totalHeads = 0
+let totalTails = 0
 document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
-
+    
     // TODO: Add event listener and handler for flip and clear buttons
+    document.getElementById('flip').addEventListener('click', function() {
+        if(Math.random() < 0.5) {
+            document.getElementById('penny-image').src = 'assets/images/penny-heads.jpg'
+            document.getElementById('message').textContent = 'You flipped heads'
+            totalHeads++
+        } else {
+            document.getElementById('penny-image').src = 'assets/images/penny-tails.jpg'
+            document.getElementById('message').textContent = 'You flipped tails'
+            totalTails++
+        }
+        let total = totalTails + totalHeads
+        
+        let percentHeads = Math.round((totalHeads/total) * 100)
+        let percentTails = Math.round((totalTails/total) * 100)
 
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
+        document.getElementById('heads').textContent = totalHeads
+        document.getElementById('tails').textContent = totalTails
+        document.getElementById('heads-percent').textContent = percentHeads + '%'
+        document.getElementById('tails-percent').textContent = percentTails + '%'
 
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
-
+    })
+    
+    document.getElementById('clear').addEventListener('click',function() {
+        totalHeads = 0
+        totalTails = 0
+        document.getElementById('message').textContent = 'Let\'s get Rolling'
+        document.getElementById('heads').textContent = totalHeads
+        document.getElementById('tails').textContent = totalTails
+        document.getElementById('heads-percent').textContent = 0 + '%'
+        document.getElementById('tails-percent').textContent = 0 + '%'
+    })
 
     // Clear Button Click Handler
         // TODO: Reset global variables to 0
